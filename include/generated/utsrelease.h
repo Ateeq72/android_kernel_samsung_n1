283886b66713b9a4a21509655db82d78e2f43059
fuss132 <homepagechecker@gmx.de>
2012-08-26 11:54:04 +0200

    first compiled version

--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.1.10"
+#define UTS_RELEASE "3.1.10-g0c40cae-dirty"
--- usr/.initramfs_data.cpio.cmd
+++ usr/.initramfs_data.cpio.cmd
@@ -1,1 +1,1 @@
-cmd_usr/initramfs_data.cpio := /bin/bash /home/fuss132/galaxyr/kernel/scripts/gen_initramfs_list.sh -o usr/initramfs_data.cpio    /home/fuss132/galaxyr/android_ramdisk
+cmd_usr/initramfs_data.cpio := /bin/bash /home/fuss132/galaxyr/kernel/scripts/gen_initramfs_list.sh -o usr/initramfs_data.cpio   -d
