b2d28c3c4696d8519b64170240ce6c64dfc3d9e8
fuss132 <homepagechecker@gmx.de>
2012-08-27 19:09:42 +0200

    enable lower 1.2ghz oc

--- arch/arm/mach-tegra/tegra2_clocks.c
+++ arch/arm/mach-tegra/tegra2_clocks.c
@@ -2071,7 +2071,7 @@ static struct clk tegra_pll_x = {
 	#if defined(CONFIG_TEGRA_OVERCLOCK)
 	.max_rate  = 1400000000,
 	#else
-	.max_rate  = 1000000000,
+	.max_rate  = 1200000000,
 	#endif
 	.u.pll = {
 		.input_min = 2000000,
@@ -2221,7 +2221,7 @@ static struct clk tegra_clk_cclk = {
 	#if defined(CONFIG_TEGRA_OVERCLOCK)
 	.max_rate = 1400000000,
 	#else
-	.max_rate = 1000000000,
+	.max_rate = 1200000000,
 	#endif
 };
 
@@ -2241,7 +2241,7 @@ static struct clk tegra_clk_virtual_cpu = {
 	#if defined(CONFIG_TEGRA_OVERCLOCK)
 	.max_rate  = 1400000000,
 	#else
-	.max_rate  = 1000000000,
+	.max_rate  = 1200000000,
 	#endif
 	.u.cpu = {
 		.main      = &tegra_pll_x,
@@ -2666,9 +2666,9 @@ static struct tegra_sku_rate_limit sku_limits[] =
 	RATE_LIMIT("cclk",  1400000000, 0x04, 0x08, 0x0F),
 	RATE_LIMIT("pll_x",  1400000000, 0x04, 0x08, 0x0F),
 	#else
-	RATE_LIMIT("cpu",	1000000000, 0x04, 0x08, 0x0F),
-	RATE_LIMIT("cclk",	1000000000, 0x04, 0x08, 0x0F),
-	RATE_LIMIT("pll_x",	1000000000, 0x04, 0x08, 0x0F),
+	RATE_LIMIT("cpu",	1200000000, 0x04, 0x08, 0x0F),
+	RATE_LIMIT("cclk",	1200000000, 0x04, 0x08, 0x0F),
+	RATE_LIMIT("pll_x",	1200000000, 0x04, 0x08, 0x0F),
 	#endif
 
 	RATE_LIMIT("cpu",	1200000000, 0x14, 0x17, 0x18, 0x1B, 0x1C),
